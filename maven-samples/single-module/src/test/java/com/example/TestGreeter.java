f517783670819b04fe8caad6cd41a11ad3595dc7
viji85 <smile.viji@gmail.com>
2025-07-28 18:53:56 +0530

    update failed testcase

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -35,7 +35,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
   @Test
-  public void intentionally failed() {
+  public void intentionallyfailed() {
     assertTrue(false);
   }
 
